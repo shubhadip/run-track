b537ce14a88ebe55288d4e63dc63a1628401dc1f
shubhadip <shubhadip.maity@gmail.com>
2022-02-19 11:34:22 +0530

    refactor: router changes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '', 'dist', 'index.html'));
+});
+
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
